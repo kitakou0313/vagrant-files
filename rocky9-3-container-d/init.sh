fbaca155d5b50cda01ea38442249bb32ab554a90
kitakou0313 <kitakou0313@gmail.com>
2024-03-31 17:59:14 +0900

    add: add scripts to init

--- rocky9-3-container-d/init.sh
+++ rocky9-3-container-d/init.sh
@@ -3,3 +3,7 @@
 # Install containerd + nerdctl
 wget https://github.com/containerd/nerdctl/releases/download/v1.7.5/nerdctl-full-1.7.5-linux-amd64.tar.gz
 tar Cxzvvf /usr/local nerdctl-full-1.7.5-linux-amd64.tar.gz
+
+# edit /etc/sudoers
+sed -i 's/Defaults[[:space:]]\+secure_path/# &/' /etc/sudoers
+echo 'Defaults    env_keep += "PATH"' >> /etc/sudoers
