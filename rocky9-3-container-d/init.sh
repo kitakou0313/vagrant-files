32a213f0a02be488377d6bbe72a50c72ac800dd6
kitakou0313 <kitakou0313@gmail.com>
2024-04-01 22:35:57 +0900

    add: add settings for password auth

--- rocky9-3-container-d/init.sh
+++ rocky9-3-container-d/init.sh
@@ -7,3 +7,11 @@ tar Cxzvvf /usr/local nerdctl-full-1.7.5-linux-amd64.tar.gz
 # edit /etc/sudoers
 sed -i 's/Defaults[[:space:]]\+secure_path/# &/' /etc/sudoers
 echo 'Defaults    env_keep += "PATH"' >> /etc/sudoers
+
+# create user for ssh
+useradd -m -s /bin/bash test
+echo "test:test" | sudo chpasswd
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+systemctl restart sshd
