f27bc1f0cefa454b0cc6475b0c41f1f5c038ed48
kitakou0313 <kitakou0313@gmail.com>
2024-04-04 22:56:53 +0900

    add: refactor

--- rocky9-3-container-d/init.sh
+++ rocky9-3-container-d/init.sh
@@ -7,14 +7,13 @@ tar Cxzvvf /usr/local nerdctl-full-1.7.5-linux-amd64.tar.gz
 # edit /etc/sudoers
 sed -i 's/Defaults[[:space:]]\+secure_path/# &/' /etc/sudoers
 echo 'Defaults    env_keep += "PATH"' >> /etc/sudoers
+echo "test ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 
 # create user for ssh
 useradd -m -s /bin/bash test
 echo "test:test" | sudo chpasswd
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-
-echo "test ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 
 systemctl restart sshd
 systemctl enable containerd
