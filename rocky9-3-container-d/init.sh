1b095c14d69e5c5b80f93db79e78c7497c68359a
kitakou0313 <kitakou0313@gmail.com>
2024-04-04 23:00:14 +0900

    add:install my dotfile

--- rocky9-3-container-d/init.sh
+++ rocky9-3-container-d/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install my dot file
+wget https://raw.githubusercontent.com/kitakou0313/dotfiles/main/installer.sh
+/bin/bash installer.sh
+
 # Install containerd + nerdctl
 wget https://github.com/containerd/nerdctl/releases/download/v1.7.5/nerdctl-full-1.7.5-linux-amd64.tar.gz
 tar Cxzvvf /usr/local nerdctl-full-1.7.5-linux-amd64.tar.gz
@@ -15,5 +19,7 @@ echo "test:test" | sudo chpasswd
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
-systemctl restart sshd
 systemctl enable containerd
+
+systemctl restart containerd
+systemctl restart sshd
